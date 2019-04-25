2d50e6c1eef3bf7785d8d6d0a5974ed841703441
ghostnoop <giliyazovmarat@gmail.com>
2019-04-25 22:41:07 +0300

    Create index.php

---
+++ index.php
@@ -0,0 +1,12 @@
+<?php
+$title = 'PHP: пример простого сайта';
+$body  = ' Наш самый простой сайт';
+?>
+<html>
+<head>
+    <title><?php echo $title;?></title>
+</head>
+<body>
+<?php echo $body;?>
+</body>
+</html>
